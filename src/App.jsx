8a4493e4c8deb4a2b5b096d32822d344a3d0a3b1
AdnenFarhani-New <89908357+AdnenFarhani-AOS@users.noreply.github.com>
2022-02-21 04:11:42 +0100

    feat: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FilmList from "./Components/Film/FilmList";
+import FilmDetails from "./Components/Film/FilmDetails";
 export default class App extends Component {
   render() {
-    return "";
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FilmList />} />
+          <Route
+            path="/film/:slug"
+            render={(props) => <FilmDetails {...props} />}
+            element={<FilmDetails />}
+          />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
